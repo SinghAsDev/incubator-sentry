7ff163ddc3c1c1eaad2a1cf4723bbc29ea21ee73
Ashish Singh <asingh@cloudera.com>
2016-02-19 12:21:07 -0800

    Make sentry clients in Kafka Authorizer use kerberos auth by default

--- sentry-binding/sentry-binding-kafka/src/main/java/org/apache/sentry/kafka/authorizer/SentryKafkaAuthorizer.java
+++ sentry-binding/sentry-binding-kafka/src/main/java/org/apache/sentry/kafka/authorizer/SentryKafkaAuthorizer.java
@@ -18,54 +18,31 @@ package org.apache.sentry.kafka.authorizer;
 
 import kafka.network.RequestChannel;
 import kafka.security.auth.Acl;
-import kafka.security.auth.Allow;
-import kafka.security.auth.Allow$;
 import kafka.security.auth.Authorizer;
 import kafka.security.auth.Operation;
-import kafka.security.auth.Operation$;
 import kafka.security.auth.Resource;
-import kafka.security.auth.ResourceType$;
-import org.apache.hadoop.security.UserGroupInformation;
-import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.security.auth.KafkaPrincipal;
-import org.apache.sentry.SentryUserException;
-import org.apache.sentry.core.common.Authorizable;
-import org.apache.sentry.core.model.kafka.KafkaAuthorizable;
-import org.apache.sentry.kafka.ConvertUtil;
 import org.apache.sentry.kafka.binding.KafkaAuthBinding;
 import org.apache.sentry.kafka.binding.KafkaAuthBindingSingleton;
 import org.apache.sentry.kafka.conf.KafkaAuthConf;
-import org.apache.sentry.provider.db.generic.service.thrift.SentryGenericServiceClient;
-import org.apache.sentry.provider.db.generic.service.thrift.SentryGenericServiceClientFactory;
-import org.apache.sentry.provider.db.generic.service.thrift.TAuthorizable;
-import org.apache.sentry.provider.db.generic.service.thrift.TSentryPrivilege;
-import org.apache.sentry.provider.db.generic.service.thrift.TSentryRole;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import scala.Option;
-import scala.Predef;
-import scala.Tuple2;
-import scala.collection.Iterator;
-import scala.collection.JavaConversions;
 import scala.collection.immutable.Map;
 import scala.collection.immutable.Set;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.HashMap;
 
 
 public class SentryKafkaAuthorizer implements Authorizer {
 
-  private static Logger LOG = LoggerFactory.getLogger(SentryKafkaAuthorizer.class);
-  private final String COMPONENT_NAME = KafkaAuthConf.AuthzConfVars.getDefault(KafkaAuthConf.KAFKA_SERVICE_INSTANCE_NAME);
+  private final static Logger LOG = LoggerFactory.getLogger(SentryKafkaAuthorizer.class);
+  private final static String COMPONENT_NAME = KafkaAuthConf.AuthzConfVars.getDefault(KafkaAuthConf.KAFKA_SERVICE_INSTANCE_NAME);
 
   private KafkaAuthBinding binding;
-  private KafkaAuthConf kafkaAuthConf;
   private String kafkaServiceInstanceName = COMPONENT_NAME;
+  private String requestorName = KafkaAuthConf.AuthzConfVars.getDefault(KafkaAuthConf.KAFKA_SERVICE_USER_NAME);
 
   String sentry_site = null;
   List<KafkaPrincipal> super_users = null;
@@ -84,99 +61,36 @@ public class SentryKafkaAuthorizer implements Authorizer {
     }
     LOG.debug("User: " + user + " is not a SuperUser");
     return binding.authorize(session, operation, resource);
-  }
+}
 
   @Override
   public void addAcls(Set<Acl> acls, final Resource resource) {
-    verifyAcls(acls);
-    LOG.info("Adding Acl: acl->" + acls + " resource->" + resource);
-
-    final UserGroupInformation loginUser = getLoginUser();
-    final Iterator<Acl> iterator = acls.iterator();
-    while (iterator.hasNext()) {
-      final Acl acl = iterator.next();
-      final String role = getRole(acl);
-      if (!roleExists(role)) {
-        throw new KafkaException("Can not add Acl for non-existent Role: " + role);
-      }
-      execute(new Command<Void>() {
-        @Override
-        public Void run(SentryGenericServiceClient client) throws Exception {
-          client.grantPrivilege(
-              loginUser.getShortUserName(), role, COMPONENT_NAME, toTSentryPrivilege(acl, resource));
-          return null;
-        }
-      });
-    }
+    binding.addAcls(acls, resource);
   }
 
   @Override
   public boolean removeAcls(Set<Acl> acls, final Resource resource) {
-    verifyAcls(acls);
-    LOG.info("Removing Acl: acl->" + acls + " resource->" + resource);
-    final UserGroupInformation loginUser = getLoginUser();
-    final Iterator<Acl> iterator = acls.iterator();
-    while (iterator.hasNext()) {
-      final Acl acl = iterator.next();
-      final String role = getRole(acl);
-      execute(new Command<Void>() {
-        @Override
-        public Void run(SentryGenericServiceClient client) throws Exception {
-          client.dropPrivilege(
-              loginUser.getShortUserName(), role, toTSentryPrivilege(acl, resource));
-          return null;
-        }
-      });
-    }
-
-    return true;
+    return binding.removeAcls(acls, resource);
   }
 
   @Override
   public boolean removeAcls(final Resource resource) {
-    LOG.info("Removing Acls for Resource: resource->" + resource);
-    final UserGroupInformation loginUser = getLoginUser();
-    List<String> roles = getAllRoles();
-    final List<TSentryPrivilege> tSentryPrivileges = getAllPrivileges(roles);
-    execute(new Command<Void>() {
-      @Override
-      public Void run(SentryGenericServiceClient client) throws Exception {
-        for (TSentryPrivilege tSentryPrivilege: tSentryPrivileges) {
-          if (isPrivilegeForResource(tSentryPrivilege, resource)) {
-            client.dropPrivilege(
-                loginUser.getShortUserName(), COMPONENT_NAME, tSentryPrivilege);
-          }
-        }
-        return null;
-      }
-    });
-
-    return true;
+    return binding.removeAcls(resource);
   }
 
   @Override
   public Set<Acl> getAcls(Resource resource) {
-    final Option<Set<Acl>> acls = getAcls().get(resource);
-    if (acls.nonEmpty())
-      return acls.get();
-    return new scala.collection.immutable.HashSet<Acl>();
+    return binding.getAcls(resource);
   }
 
   @Override
   public Map<Resource, Set<Acl>> getAcls(KafkaPrincipal principal) {
-    if (principal.getPrincipalType().toLowerCase().equals("group")) {
-      List<String> roles = getRolesforGroup(principal.getName());
-      return getAclsForRoles(roles);
-    } else {
-      LOG.info("Did not recognize Principal type: " + principal.getPrincipalType() + ". Returning Acls for all principals.");
-      return getAcls();
-    }
+    return binding.getAcls(principal);
   }
 
   @Override
   public Map<Resource, Set<Acl>> getAcls() {
-    final List<String> roles = getAllRoles();
-    return getAclsForRoles(roles);
+    return binding.getAcls();
   }
 
   @Override
@@ -197,53 +111,14 @@ public class SentryKafkaAuthorizer implements Authorizer {
     if (kafkaServiceInstanceName != null) {
       this.kafkaServiceInstanceName = kafkaServiceInstanceName.toString();
     }
+    final Object kafkaServiceUserName = configs.get(KafkaAuthConf.KAFKA_SERVICE_USER_NAME);
+    if (kafkaServiceUserName != null) {
+      this.requestorName = kafkaServiceUserName.toString();
+    }
     LOG.info("Configuring Sentry KafkaAuthorizer: " + sentry_site);
     final KafkaAuthBindingSingleton instance = KafkaAuthBindingSingleton.getInstance();
-    instance.configure(this.kafkaServiceInstanceName, sentry_site);
+    instance.configure(this.kafkaServiceInstanceName, this.requestorName, sentry_site);
     this.binding = instance.getAuthBinding();
-    this.kafkaAuthConf = instance.getKafkaAuthConf();
-  }
-
-  public void addRole(final String role) {
-    if (roleExists(role)) {
-      throw new KafkaException("Can not create an existing role, " + role + ", again.");
-    }
-
-    final UserGroupInformation loginUser = getLoginUser();
-    execute(new Command<Void>() {
-      @Override
-      public Void run(SentryGenericServiceClient client) throws Exception {
-        client.createRole(
-            loginUser.getShortUserName(), role, COMPONENT_NAME);
-        return null;
-      }
-    });
-  }
-
-  public void addRoleToGroups(final String role, final java.util.Set<String> groups) {
-    final UserGroupInformation loginUser = getLoginUser();
-    execute(new Command<Void>() {
-      @Override
-      public Void run(SentryGenericServiceClient client) throws Exception {
-        client.addRoleToGroups(
-            loginUser.getShortUserName(), role, COMPONENT_NAME, groups);
-        return null;
-      }
-    });
-  }
-
-  public void dropAllRoles() {
-    final List<String> roles = getAllRoles();
-    final UserGroupInformation loginUser = getLoginUser();
-    execute(new Command<Void>() {
-      @Override
-      public Void run(SentryGenericServiceClient client) throws Exception {
-        for (String role: roles) {
-          client.dropRole(loginUser.getShortUserName(), role, COMPONENT_NAME);
-        }
-        return null;
-      }
-    });
   }
 
   private void getSuperUsers(String kafkaSuperUsers) {
@@ -269,193 +144,15 @@ public class SentryKafkaAuthorizer implements Authorizer {
     return false;
   }
 
-  private List<String> getRolesforGroup(final String groupName) {
-    final List<String> roles = new ArrayList<>();
-    final UserGroupInformation loginUser = getLoginUser();
-    execute(new Command<Void>() {
-      @Override
-      public Void run(SentryGenericServiceClient client) throws Exception {
-        for( TSentryRole tSentryRole: client.listRolesByGroupName(loginUser.getShortUserName(), groupName, COMPONENT_NAME)) {
-          roles.add(tSentryRole.getRoleName());
-        }
-        return null;
-      }
-    });
-
-    return roles;
+  public void addRole(String role) {
+    binding.addRole(role);
   }
 
-  private SentryGenericServiceClient getClient() throws Exception {
-    return SentryGenericServiceClientFactory.create(this.kafkaAuthConf);
+  public void addRoleToGroups(String role, java.util.Set<String> groups) {
+    binding.addRoleToGroups(role, groups);
   }
 
-  /**
-   * A Command is a closure used to pass a block of code from individual
-   * functions to execute, which centralizes connection error
-   * handling. Command is parameterized on the return type of the function.
-   */
-  private interface Command<T> {
-    T run(SentryGenericServiceClient client) throws Exception;
-  }
-
-  private <T> T execute(Command<T> cmd) throws KafkaException {
-    SentryGenericServiceClient client = null;
-    try {
-      client = getClient();
-      return cmd.run(client);
-    } catch (SentryUserException ex) {
-      String msg = "Unable to excute command on sentry server: " + ex.getMessage();
-      LOG.error(msg, ex);
-      throw new KafkaException(msg, ex);
-    } catch (Exception ex) {
-      String msg = "Unable to obtain client:" + ex.getMessage();
-      LOG.error(msg, ex);
-      throw new KafkaException(msg, ex);
-    } finally {
-      if (client != null) {
-        client.close();
-      }
-    }
-  }
-
-  private TSentryPrivilege toTSentryPrivilege(Acl acl, Resource resource) {
-    final List<Authorizable> authorizables = ConvertUtil.convertResourceToAuthorizable(acl.host(), resource);
-    final List<TAuthorizable> tAuthorizables = new ArrayList<>();
-    for (Authorizable authorizable: authorizables) {
-      tAuthorizables.add(new TAuthorizable(authorizable.getTypeName(), authorizable.getName()));
-    }
-    TSentryPrivilege tSentryPrivilege = new TSentryPrivilege(COMPONENT_NAME, kafkaServiceInstanceName, tAuthorizables, acl.operation().name());
-    return tSentryPrivilege;
-  }
-
-  private String getRole(Acl acl) {
-    return acl.principal().getName();
-  }
-
-  private UserGroupInformation getLoginUser() {
-    UserGroupInformation loginUser = null;
-    try {
-      loginUser = UserGroupInformation.getLoginUser();
-    } catch (IOException e) {
-      throw new KafkaException("Failed to get login user information.", e);
-    }
-
-    return loginUser;
-  }
-
-  private void verifyAcls(Set<Acl> acls) {
-    final Iterator<Acl> iterator = acls.iterator();
-    while (iterator.hasNext()) {
-      final Acl acl = iterator.next();
-      assert acl.principal().getPrincipalType().toLowerCase().equals("role") : "Only Acls with KafkaPrincipal of type \"role;\" is supported.";
-      assert acl.permissionType().name().equals(Allow.name()): "Only Acls with Permission of type \"Allow\" is supported.";
-    }
-  }
-
-  private boolean isPrivilegeForResource(TSentryPrivilege tSentryPrivilege, Resource resource) {
-    final java.util.Iterator<TAuthorizable> authorizablesIterator = tSentryPrivilege.getAuthorizablesIterator();
-    while (authorizablesIterator.hasNext()) {
-      TAuthorizable tAuthorizable = authorizablesIterator.next();
-      if (tAuthorizable.getType().equals(resource.resourceType().name())) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private List<TSentryPrivilege> getAllPrivileges(final List<String> roles) {
-    final List<TSentryPrivilege> tSentryPrivileges = new ArrayList<>();
-    final UserGroupInformation loginUser = getLoginUser();
-    execute(new Command<Void>() {
-      @Override
-      public Void run(SentryGenericServiceClient client) throws Exception {
-        for (String role: roles) {
-          tSentryPrivileges.addAll(client.listPrivilegesByRoleName(
-              loginUser.getShortUserName(), role, COMPONENT_NAME, kafkaServiceInstanceName));
-        }
-        return null;
-      }
-    });
-
-    return tSentryPrivileges;
-  }
-
-  private List<String> getAllRoles() {
-    final List<String> roles = new ArrayList<>();
-    final UserGroupInformation loginUser = getLoginUser();
-    execute(new Command<Void>() {
-      @Override
-      public Void run(SentryGenericServiceClient client) throws Exception {
-        for( TSentryRole tSentryRole: client.listAllRoles(loginUser.getShortUserName(), COMPONENT_NAME)) {
-          roles.add(tSentryRole.getRoleName());
-        }
-        return null;
-      }
-    });
-
-    return roles;
-  }
-
-  private Map<Resource, Set<Acl>> getAclsForRoles(final List<String> roles) {
-    final java.util.Map<Resource, Set<Acl>> resourceAclsMap = new HashMap<Resource, Set<Acl>>();
-    final java.util.Map<String, Set<TSentryPrivilege>> rolePrivilegesMap = new HashMap<String, Set<TSentryPrivilege>>();
-    final UserGroupInformation loginUser = getLoginUser();
-    execute(new Command<Void>() {
-      @Override
-      public Void run(SentryGenericServiceClient client) throws Exception {
-        for (String role: roles) {
-          final java.util.Set<TSentryPrivilege> rolePrivileges = client.listPrivilegesByRoleName(
-              loginUser.getShortUserName(), role, COMPONENT_NAME, kafkaServiceInstanceName);
-          final Set<TSentryPrivilege> rolePrivilegesScala =
-              scala.collection.JavaConverters.asScalaSetConverter(rolePrivileges).asScala().toSet();
-          rolePrivilegesMap.put(role, rolePrivilegesScala);
-        }
-        return null;
-      }
-    });
-
-    for (String role: rolePrivilegesMap.keySet()) {
-      Set<TSentryPrivilege> privileges = rolePrivilegesMap.get(role);
-      final Iterator<TSentryPrivilege> iterator = privileges.iterator();
-      while (iterator.hasNext()) {
-        TSentryPrivilege privilege = iterator.next();
-        final List<TAuthorizable> authorizables = privilege.getAuthorizables();
-        String host = null;
-        String operation = privilege.getAction();
-        for (TAuthorizable tAuthorizable: authorizables) {
-          if (tAuthorizable.getType().equals(KafkaAuthorizable.AuthorizableType.HOST.name())) {
-            host = tAuthorizable.getName();
-          } else {
-            Resource resource = new Resource(ResourceType$.MODULE$.fromString(tAuthorizable.getType()), tAuthorizable.getName());
-            if (operation.equals("*")) {
-              operation = "All";
-            }
-            Acl acl = new Acl(new KafkaPrincipal("role", role), Allow$.MODULE$, host, Operation$.MODULE$.fromString(operation));
-            java.util.Set<Acl> newAclsJava = new HashSet<>();
-            newAclsJava.add(acl);
-            addExistingAclsForResource(resourceAclsMap, resource, newAclsJava);
-            final scala.collection.mutable.Set<Acl> aclScala = JavaConversions.asScalaSet(newAclsJava);
-            resourceAclsMap.put(resource, aclScala.<Acl>toSet());
-          }
-        }
-      }
-    }
-
-    return scala.collection.JavaConverters.mapAsScalaMapConverter(resourceAclsMap).asScala().toMap(Predef.<Tuple2<Resource, Set<Acl>>>conforms());
-  }
-
-  private void addExistingAclsForResource(java.util.Map<Resource, Set<Acl>> resourceAclsMap, Resource resource, java.util.Set<Acl> newAclsJava) {
-    final Set<Acl> existingAcls = resourceAclsMap.get(resource);
-    if (existingAcls != null) {
-      final Iterator<Acl> aclsIter = existingAcls.iterator();
-      while (aclsIter.hasNext()) {
-        Acl curAcl = aclsIter.next();
-        newAclsJava.add(curAcl);
-      }
-    }
-  }
-
-  private boolean roleExists(String role) {
-    return getAllRoles().contains(role);
+  public void dropAllRoles() {
+    binding.dropAllRoles();
   }
 }
---
+++ sentry-tests/sentry-tests-kafka/src/main/java/org/apache/sentry/tests/e2e/kafka/KafkaTestServer.java
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.sentry.tests.e2e.kafka;
+
+import kafka.server.KafkaServerStartable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
+public class KafkaTestServer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaTestServer.class);
+
+    private int zkPort = -1;
+    private int kafkaPort = -1;
+    private EmbeddedZkServer zkServer = null;
+    private KafkaServerStartable kafkaServer = null;
+    private File sentrySitePath = null;
+
+    public KafkaTestServer(File sentrySitePath) throws Exception {
+        this.zkPort = getFreePort();
+        this.kafkaPort = getFreePort();
+        this.sentrySitePath = sentrySitePath;
+        createZkServer();
+        createKafkaServer();
+    }
+
+    public void start() throws Exception {
+        kafkaServer.startup();
+        LOGGER.info("Started Kafka broker.");
+    }
+
+    public void shutdown() {
+        kafkaServer.shutdown();
+        kafkaServer.awaitShutdown();
+        LOGGER.info("Stopped Kafka server.");
+
+        try {
+            zkServer.shutdown();
+            LOGGER.info("Stopped ZK server.");
+        } catch (IOException e) {
+            LOGGER.error("Failed to shutdown ZK server.", e);
+        }
+    }
+
+    public static int getFreePort() {
+        ServerSocket zkServerSocket = null;
+        try {
+            zkServerSocket = new ServerSocket(0);
+        } catch (IOException e) {
+            LOGGER.error("Failed to open port for Zookeeper server.", e);
+        }
+        return zkServerSocket.getLocalPort();
+    }
+
+    private Path getTempDirectory() {
+        Path tempDirectory = null;
+        try {
+            tempDirectory = Files.createTempDirectory("kafka-sentry-");
+        } catch (IOException e) {
+            LOGGER.error("Failed to create temp dir for Kafka's log dir.", e);
+        }
+        return tempDirectory;
+    }
+
+    private void generateRequiredKafkaProps(Properties props) throws UnknownHostException {
+        props.put("listeners", "SSL://localhost:" + kafkaPort);
+        props.put("log.dir", getTempDirectory().toAbsolutePath().toString());
+        props.put("zookeeper.connect", InetAddress.getLocalHost().getHostAddress() + ":" + zkPort);
+        props.put("replica.socket.timeout.ms", "1500");
+        props.put("controller.socket.timeout.ms", "1500");
+        props.put("controlled.shutdown.enable", true);
+        props.put("delete.topic.enable", false);
+        props.put("controlled.shutdown.retry.backoff.ms", "100");
+        props.put("port", kafkaPort);
+        props.put("authorizer.class.name", "org.apache.sentry.kafka.authorizer.SentryKafkaAuthorizer");
+        props.put("sentry.kafka.site.url", "file://" + sentrySitePath.getAbsolutePath());
+        props.put("allow.everyone.if.no.acl.found", "true");
+        props.put("ssl.keystore.location", KafkaTestServer.class.getResource("/test.keystore.jks").getPath());
+        props.put("ssl.keystore.password", "test-ks-passwd");
+        props.put("ssl.key.password", "test-key-passwd");
+        props.put("ssl.truststore.location", KafkaTestServer.class.getResource("/test.truststore.jks").getPath());
+        props.put("ssl.truststore.password", "test-ts-passwd");
+        props.put("security.inter.broker.protocol", "SSL");
+        props.put("ssl.client.auth", "required");
+        props.put("kafka.superusers", "User:CN=superuser;User:CN=superuser1; User:CN=Superuser2 ");
+        //props.put("principal.builder.class", "org.apache.sentry.tests.e2e.kafka.CustomPrincipalBuilder");
+    }
+
+    private void createKafkaServer() throws UnknownHostException {
+        Properties props = new Properties();
+        generateRequiredKafkaProps(props);
+        kafkaServer = KafkaServerStartable.fromProps(props);
+    }
+
+    private void createZkServer() throws Exception {
+        try {
+            zkServer = new EmbeddedZkServer(zkPort);
+            zkPort = zkServer.getZk().getClientPort();
+        } catch (Exception e) {
+            LOGGER.error("Failed to create testing zookeeper server.", e);
+        }
+    }
+
+    public String getBootstrapServers() {
+        return "localhost:" + kafkaPort;
+    }
+}
